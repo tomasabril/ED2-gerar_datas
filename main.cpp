486d3081844dd7fb0b1e0e505d23c2d8279d4e86
Tomás Abril <tomasabril@users.noreply.github.com>
2015-04-26 15:17:19 -0300

    gerar datas entre 1800 e 200

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ int main()
     int i;
     FILE *f = fopen("datas.txt", "w");
     srand (time(NULL));             /* initialize random seed: */
-
-    for(i=0; i<100000; i++) {
-        ano = rand()%115 + 1900;    /* generate number between 1900 and 2015: */
+//____________1.000.000
+    for(i=0; i<1000000; i++) {
+        ano = rand()%200 + 1800;    /* generate number between 1800 and 2000: */
         mes = rand()%12 + 1;
         dia = rand()%30 + 1;
         data = ano*10000 + mes*100 + dia;
